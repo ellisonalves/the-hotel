267c659017c3fcc66a8cf2ddc82f866ef9be8be1
Ellison Alves <ellisonalves@gmail.com>
2019-04-22 14:01:22 +0200

    creating inital structure with springboot 2. Closes #1

---
+++ src/main/java/com/ellisonalves/thehotel/TheHotelApplication.java
@@ -0,0 +1,13 @@
+package com.ellisonalves.thehotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheHotelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ellisonalves/thehotel/TheHotelApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ellisonalves.thehotel;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TheHotelApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
