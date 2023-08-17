baf8152c4449c84bf797a53dc76b05199e67b272
Ellison Alves <ellisonalves@gmail.com>
2023-08-17 13:31:53 +0200

    making tests pass

--- src/main/java/com/ellisonalves/thehotel/domain/entity/Booking.java
+++ src/main/java/com/ellisonalves/thehotel/domain/entity/Booking.java
@@ -16,87 +16,104 @@ import jakarta.persistence.Transient;
 @Entity
 public class Booking extends BaseEntity<UUID> {
 
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    private Guest guest;
-    private Room room;
-    private Instant startDate;
-    private Instant endDate;
+	private Guest guest;
+	private Room room;
+	private Instant startDate;
+	private Instant endDate;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    @Override
-    public UUID getId() {
-        return id;
-    }
+	@Id
+	@GeneratedValue(strategy = GenerationType.UUID)
+	@Override
+	public UUID getId() {
+		return id;
+	}
 
-    public void setId(UUID id) {
-        this.id = id;
-    }
+	public void setId(UUID id) {
+		this.id = id;
+	}
 
-    @ManyToOne
-    public Guest getGuest() {
-        return guest;
-    }
+	@ManyToOne
+	public Guest getGuest() {
+		return guest;
+	}
 
-    public void setGuest(Guest guest) {
-        this.guest = guest;
-    }
+	public void setGuest(Guest guest) {
+		this.guest = guest;
+	}
 
-    @ManyToOne
-    public Room getRoom() {
-        return room;
-    }
+	@ManyToOne
+	public Room getRoom() {
+		return room;
+	}
 
-    public void setRoom(Room room) {
-        this.room = room;
-    }
+	public void setRoom(Room room) {
+		this.room = room;
+	}
 
-    @Temporal(TemporalType.TIMESTAMP)
-    public Instant getStartDate() {
-        return startDate;
-    }
+	@Temporal(TemporalType.TIMESTAMP)
+	public Instant getStartDate() {
+		return startDate;
+	}
 
-    public void setStartDate(Instant startDate) {
-        this.startDate = startDate;
-    }
+	public void setStartDate(Instant startDate) {
+		this.startDate = startDate;
+	}
 
-    @Temporal(TemporalType.TIMESTAMP)
-    public Instant getEndDate() {
-        return endDate;
-    }
+	@Temporal(TemporalType.TIMESTAMP)
+	public Instant getEndDate() {
+		return endDate;
+	}
 
-    public void setEndDate(Instant endDate) {
-        this.endDate = endDate;
-    }
+	public void setEndDate(Instant endDate) {
+		this.endDate = endDate;
+	}
 
-    @Override
-    public boolean equalTo(Object o) {
-        Booking other = (Booking) o;
-        return Objects.equals(id, other.getId());
-    }
+	@Override
+	public boolean equalTo(Object o) {
+		Booking other = (Booking) o;
+		return Objects.equals(id, other.getId());
+	}
 
-    @Override
-    public int hashCodePrime() {
-        return 31;
-    }
+	@Override
+	public int hashCodePrime() {
+		return 31;
+	}
 
-    @Transient
+	@Transient
 	public boolean isMissingMandatoryFields() {
-		// TODO Auto-generated method stub
-		return false;
+		return !(hasGuestId() || hasRoomId() || hasStartDate() || hasEndDate());
 	}
 
-    @Transient
-	public boolean isStartOrEndDatesBefore(Instant now) {
-		// TODO Auto-generated method stub
-		return false;
+	@Transient
+	public boolean isStartOrEndDatesBefore(Instant instant) {
+		return hasStartAndEndDates() && startDate.isBefore(instant) || endDate.isBefore(instant);
 	}
 
-    @Transient
+	@Transient
 	public boolean isStartDateAfterEndDate() {
-		// TODO Auto-generated method stub
-		return false;
+		return hasStartAndEndDates() && startDate.isAfter(endDate);
+	}
+
+	private boolean hasEndDate() {
+		return endDate != null;
+	}
+
+	private boolean hasStartDate() {
+		return startDate != null;
+	}
+
+	private boolean hasRoomId() {
+		return room != null && room.getId() != null;
+	}
+
+	private boolean hasGuestId() {
+		return guest != null && guest.getId() != null;
+	}
+
+	private boolean hasStartAndEndDates() {
+		return hasStartDate() && hasEndDate();
 	}
 
 }
